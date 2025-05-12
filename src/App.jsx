83740a053b2853804fc2193288891c7599b9135a
maundu55 <marymaundu.mm@gmail.com>
2025-05-12 11:01:59 +0300

    add route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Navbar from "./components/Navbar"
-
+import { Routes, Route } from "react-router-dom"
+import Homepage from "./pages/Homepage"
+import ProductDetails from "./pages/ProductDetails"
 
 const App = () => {
   return (
     <div >
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/product/:id' element={<ProductDetails />}/>
+      </Routes>
     </div>
   )
 }
