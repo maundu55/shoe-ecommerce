c8d23f55ba6e65079867c287bf732e7e20b5d1f2
maundu55 <marymaundu.mm@gmail.com>
2025-05-12 09:19:40 +0300

    add component and pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline bg-amber-300">App</div>
+    <div >App</div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BiSearch } from 'react-icons/bi'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='navbar'>
+        <div>
+          <h2>LOGO HERE</h2>
+        </div>
+        <div>
+          <BiSearch />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
