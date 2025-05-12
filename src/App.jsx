8c52b8aaa1d19a15560cf381f926ff876b772f18
maundu55 <marymaundu.mm@gmail.com>
2025-05-12 09:05:22 +0300

    init shoe ecommerce site

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+
+const App = () => {
+  return (
+    <div className="text-3xl font-bold underline bg-amber-300">App</div>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,157 @@
+import sneakers1 from './assets/sneakers13.jpg'
+import sneakers2 from './assets/sneakers2.jpg'
+import sneakers3 from './assets/sneakers3.jpg'
+import sneakers4 from './assets/sneakers1.jpg'
+import sneakers5 from './assets/sneakers5.jpg'
+import sneakers6 from './assets/sneakers6.jpg'
+import sneakers7 from './assets/sneakers7.jpg'
+import sneakers8 from './assets/sneakers8.jpg'
+import sneakers9 from './assets/sneakers9.jpg'
+import sneakers10 from './assets/sneakers10.jpg'
+import sneakers11 from './assets/sneakers11.jpg'
+import sneakers12 from './assets/sneakers12.jpg'
+// import sneakers13 from './assets/sneakers.jpg'
+// import sneakers14 from './assets/sneakers.jpg'
+// import sneakers15 from './assets/sneakers.jpg'
+// import sneakers16 from './assets/sneakers.jpg'
+// import sneakers17 from './assets/sneakers.jpg'
+
+
+export const productsData = [
+    {
+        id : 1,
+        image : sneakers1,
+        title : ' Men Air Running Shoe',
+        price: 130,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 2,
+        image : sneakers2,
+        title : ' FitHit Men Non-Slippery Shoe',
+        price: 130,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 3,
+        image : sneakers3,
+        title : ' Athletic Tennis Casual Sneakers',
+        price: 130,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 4,
+        image : sneakers4,
+        title : ' Full Force Low Men Shoe',
+        price: 130,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 5,
+        image : sneakers5,
+        title : ' Lightweight Gym Sport Sneakers',
+        price: 130,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 6,
+        image : sneakers6,
+        title : ' Men Blade Fashion Tennis Sneakers',
+        price: 130,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 7,
+        image : sneakers7,
+        title : ' Men Fashion Sneakers Athletic Shoe',
+        price: 130,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 8,
+        image : sneakers8,
+        title : ' Men Fashion Sneakers Athletic Shoe',
+        price: 130,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 9,
+        image : sneakers9,
+        title : ' Men Fashion Sneakers Athletic Shoe',
+        price: 130,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 10,
+        image : sneakers10,
+        title : ' Men Fashion Sneakers Athletic Shoe',
+        price: 130,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 11,
+        image : sneakers11,
+        title : ' Men Fashion Sneakers Athletic Shoe',
+        price: 130,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 12,
+        image : sneakers12,
+        title : ' Men Fashion Sneakers Athletic Shoe',
+        price: 130,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    // {
+    //     id : 13,
+    //     image : sneakers13,
+    //     title : ' Men Fashion Sneakers Athletic Shoe',
+    //     price: 130,
+    //     category: 'ladies',
+    //     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    // },
+    // {
+    //     id : 14,
+    //     image : sneakers14,
+    //     title : ' Men Fashion Sneakers Athletic Shoe',
+    //     price: 130,
+    //     category: 'ladies',
+    //     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    // },
+    // {
+    //     id : 15,
+    //     image : sneakers15,
+    //     title : ' Men Fashion Sneakers Athletic Shoe',
+    //     price: 130,
+    //     category: 'ladies',
+    //     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    // },
+    // {
+    //     id : 16,
+    //     image : sneakers16,
+    //     title : ' Men Fashion Sneakers Athletic Shoe',
+    //     price: 130,
+    //     category: 'ladies',
+    //     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    // },
+    // {
+    //     id : 17,
+    //     image : sneakers17,
+    //     title : ' Men Fashion Sneakers Athletic Shoe',
+    //     price: 130,
+    //     category: 'ladies',
+    //     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    // },
+]
