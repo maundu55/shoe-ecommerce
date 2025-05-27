c4f1f0ee7589a3984b7f950b95b04a6ca6595bba
maundu55 <marymaundu.mm@gmail.com>
2025-05-27 19:51:17 +0300

    update hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div>
+      <div>
+        <div>
+          <h1>Step Into The Future with Our Collection</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro omnis dolor exercitationem itaque, esse et ipsam reprehenderit officia iusto nostrum culpa nisi aliquid enim minima!</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
