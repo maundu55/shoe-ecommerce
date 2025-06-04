a2dfe7a094c22edb1e6244ed67dd1491f032b5fe
maundu55 <marymaundu.mm@gmail.com>
2025-06-04 12:51:10 +0300

    consume the Shopcontext

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, {useContext} from 'react'
 
 const Product = () => {
+  const {products} = useContext(ShopContext)
   return (
-    <div>Product</div>
+    <>
+    <div>
+      <h1>Our Collecton</h1>
+    </div>
+    <div>
+      {
+        products.map((product) => (
+          <div key={product.id} className="product">
+            <img src={product.image} alt={product.name} />
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <button>Add to Cart</button>
+          </div>
+        ))
+      }
+    </div>
+    </>
   )
 }
 
